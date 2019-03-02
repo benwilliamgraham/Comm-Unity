efb36ebfc1e4edc1b9e5df5c4984104707013909
Ben Graham <benwilliamgraham@gmail.com>
2019-03-02 00:20:25 -0500

    Better Server

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
-$(document).ready(function(){    
-    var pusher = new Pusher('d8769f3993afaff26999', {
-        cluster: 'us2',
-        encrypted: false
-    });
-    let channel = pusher.subscribe('public-chat');
-    channel.bind('message-added', onMessageAdded);
-    $('#btn-chat').click(function(){
-        const message = $("#message").val();
-        $("#message").val("");
-        //send message
-        $.post( "http://localhost:8000/message", { message } );
-    });
-    function onMessageAdded(data) {
-        let template = $("#new-message").html();
-        template = template.replace("{{body}}", data.message);
-        $(".chat").append(template);
-    }
+$(document).ready(function(){
+	//connect to the node server
+	var pusher = new Pusher('d8769f3993afaff26999', {
+		cluster: 'us2',
+		encrypted: false
+	});
+
+	//create chat channel
+	let channel = pusher.subscribe('public-chat');
+	
+	channel.bind('message-added', onMessageAdded);
+
+	//add send button
+	$('#send').click(function(){
+		const message = $("#message").val();
+
+		//clear message box
+		$("#message").val("");
+
+		//send message
+		$.post( "http://localhost:8000/message", { message } );
+	});
+
+	//on new message received
+	function onMessageAdded(data) {
+		$("#chat").append(data.message + "<br>");
+	}
 });
